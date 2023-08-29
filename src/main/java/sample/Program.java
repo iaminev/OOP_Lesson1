93537df90df7179cee56e393b3485b047412786f
Ilshat Aminev <i.aminev@gmail.com>
2023-08-29 10:10:21 +0300

    init

---
+++ src/main/java/sample/Program.java
@@ -0,0 +1,14 @@
+package sample;
+
+import ru.geekbrains.lesson1.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        //Product product3 = new Product("Product3");
+        //product3.price = -100;
+        //product3.name = "A";
+        //System.out.println(product3.displayInfo());
+    }
+
+}
